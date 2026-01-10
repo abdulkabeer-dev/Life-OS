7b6cf16539d3b6aa9838bc112334a6fd244b103b
abdulkabeer-dev <ak.dev.1930@gmail.com>
2026-01-10 12:26:04 +0530

    feat: Implement Remember Me functionality

--- components/Auth.tsx
+++ components/Auth.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { useLifeOS } from '../context/LifeOSContext';
 import { Infinity, LogIn, Mail, UserPlus, ArrowRight } from 'lucide-react';
 
